c9eacff6beaea1bffc836372230ecaa0c78609e6
Godwin kpodo <kpodwinkpodo@gmail.com>
2023-02-17 14:22:15 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return; alwawys return 0 (Success)
+ * Return: alwawys return 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/** Return type is always zero*/
 return (0);
 }
