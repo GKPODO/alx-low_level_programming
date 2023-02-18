dd9dc40a5127742bbef1519f0e6af72a6853fa4c
Godwin kpodo <kpodwinkpodo@gmail.com>
2023-02-17 18:11:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - Entry point
  * Return 1 (Success)
  */
-int main (void)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1)
+	return(1);
 }
