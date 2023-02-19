af99affd4232ba2ff56dedf7cd5f7548c2a7f306
Godwin kpodo <kpodwinkpodo@gmail.com>
2023-02-19 09:51:11 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+int main(void)
+{
+		int n;
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					return (0);
+}
