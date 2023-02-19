a56ebd999f97a2955b0ecabdc8191bb8bab3be05
Godwin kpodo <kpodwinkpodo@gmail.com>
2023-02-19 06:19:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success) This show that the program i successfll
+ * main - A c program that prints a line to the standard erro
+ * Return: 1 (Success) This show that the program i successfll
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout
-			);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
